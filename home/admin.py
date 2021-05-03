b27ecee994e0a92722f4f574f9fb7a004779d9fb
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-05-03 19:45:01 +0430

    end

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+
+@admin.register(Message)
+class AdminMessage(admin.ModelAdmin):
+    list_display = ['username', 'room']
--- home/views.py
+++ home/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+
+def room(request, room_name):
+    username = request.GET.get('username', 'Anonymous')
+    return render(request, 'home/room.html', {'room_name': room_name, 'username': username})
